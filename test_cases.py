f88b55637d572b82bd2800a060392a31b93fd358
68813044 <qiujiongli@gmail.com>
2023-07-14 10:13:47 +0000

    fix

--- test_cases.py
+++ test_cases.py
@@ -1,10 +1,23 @@
 import fuzzy_match
+import time
 from PIL import Image
 
+def calculate_time(func):
 
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        print(f"Function '{func.__name__}' executed in {execution_time} seconds.")
+        return result
+
+    return wrapper
+
+@calculate_time
 def test_case_1():
-    path1 = 'images\set1_a.png'
-    path2 = 'images\set1_b.png'
+    path1 = 'images/set1_a.png'
+    path2 = 'images/set1_b.png'
 
     image_1 = Image.open(path1)
     image_2 = Image.open(path2)
@@ -19,9 +32,10 @@ def test_case_1():
 
     print('test case 1: ', res)
 
+@calculate_time
 def test_case_2():
-    path1 = 'images\set2_a.png'
-    path2 = 'images\set2_b.png'
+    path1 = 'images/set2_a.png'
+    path2 = 'images/set2_b.png'
 
     image_1 = Image.open(path1)
     image_2 = Image.open(path2)
@@ -36,10 +50,10 @@ def test_case_2():
 
     print('test case 2: ', res)
 
-
+@calculate_time
 def test_case_3():
-    path1 = 'images\set3_a.png'
-    path2 = 'images\set3_b.png'
+    path1 = 'images/set3_a.png'
+    path2 = 'images/set3_b.png'
 
     image_1 = Image.open(path1)
     image_2 = Image.open(path2)
@@ -54,9 +68,10 @@ def test_case_3():
 
     print('test case 3: ', res)
 
+@calculate_time
 def test_case_4():
-    path1 = 'images\set1_a.png'
-    path2 = 'images\set2_a.png'
+    path1 = 'images/set1_a.png'
+    path2 = 'images/set2_a.png'
 
     image_1 = Image.open(path1)
     image_2 = Image.open(path2)
